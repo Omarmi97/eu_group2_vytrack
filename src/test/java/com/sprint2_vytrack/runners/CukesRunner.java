dec987107b198e523ec69d80e569da025858ade2
eezgiarac <eezgiarac@gmail.com>
2024-08-23 11:49:41 +0200

    updated

--- src/test/java/com/sprint2_vytrack/runners/CukesRunner.java
+++ src/test/java/com/sprint2_vytrack/runners/CukesRunner.java
@@ -9,16 +9,12 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/sprint2_vytrack/step_definitions",
         dryRun = false,
-
-        tags = "@B34G2-223",
-        tags = "@storemanager",
-
+        tags = "@CreateEvent",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
